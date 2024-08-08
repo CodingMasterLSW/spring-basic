56b7b1fe3bc2f0c40d751d57f50267727a998955
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-08 16:29:14 +0900

    TEST : appConfig를 통한 DI로 테스트 코드 리팩토링

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -2,12 +2,20 @@ package hello.core.member;
 
 import static org.assertj.core.api.Assertions.*;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
 
