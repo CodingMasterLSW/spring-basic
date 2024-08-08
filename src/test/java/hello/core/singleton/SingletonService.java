62c8461e712c24a6b996f201f859498ba5f7229e
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-09 02:03:26 +0900

    TEST : 싱글톤 컨테이너를 사용한 참조값 비교

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -2,9 +2,13 @@ package hello.core.singleton;
 
 import static org.assertj.core.api.Assertions.*;
 
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonService {
 
@@ -33,6 +37,19 @@ public class SingletonService {
         assertThat(instance1).isEqualTo(instance2);
 
         instance1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 1. 조회 : 호출할 때 마다 같은 객체를 반환
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        assertThat(memberService).isSameAs(memberService2);
+
     }
 
 }
