9e9a814fd52551366bbb6bc2d56a916df9b85976
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-08 16:10:03 +0900

    REFACTOR : DIP 지키기 위한 AppConfig 추가

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import hello.core.order.RateDiscountPolicy;
+
+public class AppConfig {
+
+    public MemberService memberService(){
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService(){
+        return new OrderServiceImpl(new MemoryMemberRepository(), new RateDiscountPolicy());
+    }
+}
