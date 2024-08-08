5c2c8284e82cb677bd4bc8504282f3a97c3e43a6
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-08 16:23:19 +0900

    REFACTOR : 기존 MemberApp, OrderApp DI로 리팩토링

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,9 +1,12 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
+
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
