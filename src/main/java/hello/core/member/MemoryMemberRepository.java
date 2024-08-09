3237ca06d59b9406f7aec8e4de9a65cbda3f1303
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-09 14:13:31 +0900

    REFACTOR : MemoryMemberRepository Component 추가

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -2,7 +2,9 @@ package hello.core.member;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
