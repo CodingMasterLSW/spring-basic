b7426cf36d039e3a5ffb73071eeb7fe93fa9447b
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-10 20:38:17 +0900

    TEST : 초기화, 소멸메서드로 래픽토링

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,17 +1,13 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
+
     public void setUrl(String url){
         this.url = url;
     }
@@ -28,14 +24,13 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void destroy() throws Exception {
-        disconnect();
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
+    }
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
