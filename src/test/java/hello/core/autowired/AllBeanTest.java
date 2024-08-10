bcf41e0901bdb5f108337ef004a34177df823311
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-08-10 14:44:42 +0900

    TEST: 여러 빈 조회 테스트코드 작성

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,48 @@
+package hello.core.autowired;
+
+import static org.assertj.core.api.Assertions.*;
+
+import hello.core.AutoAppConfig;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.order.DiscountPolicy;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(discountPrice).isEqualTo(1000);
+    }
+
+    static class DiscountService {
+        private final Map<String, DiscountPolicy> policyMap;
+
+        @Autowired
+        public DiscountService(Map<String, DiscountPolicy> policyMap) {
+            this.policyMap = policyMap;
+            System.out.println("policyMap = " + policyMap);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+
+            System.out.println("discountCode = " + discountCode);
+            System.out.println("discountPolicy = " + discountPolicy);
+            return discountPolicy.discount(member, price);
+        }
+    }
+
+}
